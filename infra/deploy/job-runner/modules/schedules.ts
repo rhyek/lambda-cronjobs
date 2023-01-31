c0846d16da90f9d4d8a010d133a7e066004ea4d8
Carlos González <carlos.rgn@gmail.com>
2023-01-31 11:23:22 -0600

    disable schedules

--- infra/deploy/job-runner/modules/schedules.ts
+++ infra/deploy/job-runner/modules/schedules.ts
@@ -5,11 +5,11 @@ import { resourceName } from './resource-name';
 import { queue } from './sqs';
 
 const jobSchedules: { jobName: JobName; scheduleExpression: string }[] = [
-  {
-    jobName: JobName.CHECK_MEXICAN_EMBASSY_VISA_APPOINTMENT_AVAILABILITY,
-    scheduleExpression:
-      'cron(0 1,13,16,19,22 ? * * *)' /* every 3 hours during the day */,
-  },
+  // {
+  //   jobName: JobName.CHECK_MEXICAN_EMBASSY_VISA_APPOINTMENT_AVAILABILITY,
+  //   scheduleExpression:
+  //     'cron(0 1,13,16,19,22 ? * * *)' /* every 3 hours during the day */,
+  // },
   // {
   //   jobName: JobName.GOOGLE_TITLE,
   //   // scheduleExpression: 'cron(0/10 * ? * * *)' /* every 10 minutes */,
