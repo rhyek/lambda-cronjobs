6c9168c9e9e7872fe7957150a7a20596da86bc60
Carlos González <carlos.rgn@gmail.com>
2023-06-02 22:19:09 -0600

    delete unused job

--- apps/job-runner/src/jobs/banco-industrial-ynab-liason/banco-industrial-ynab-handle-card-charge.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from 'playwright';
-import { PlaywrightJob } from '../../playwright-job.js';
-
-export class BancoIndustrialYnabHandleCardCharge extends PlaywrightJob {
-  protected playwrightRun({ page }: { page: Page }): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-}
